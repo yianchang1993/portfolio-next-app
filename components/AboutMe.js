9ebc3b158b5aa73629f29dd3cbca471dce73ca96
Andy Chang <andy.chang@mycommunitydirectory.com.au>
2022-01-29 23:30:44 +1000

    fix styling

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -4,7 +4,7 @@ import DP from '../img/DP.svg'
 
 function AboutMe() {
   return (
-    <div className="md:flex justify-center ">
+    <div className="lg:flex justify-center ">
       <div className="flex items-center p-3 justify-center">
         <img
           className="border-2 rounded-full border-white w-70 h-70 md:w-48 md:h-48
@@ -15,7 +15,7 @@ function AboutMe() {
         />
       </div>
       <div className="w-75 p-10 md:w-2/6">
-        <h2 className="text-4xl font-bold  md:w-96 break-words">Yi-An (Andy) Chang</h2>
+        <h2 className="text-3xl font-bold md:text-4xl  md:w-96 break-words">Yi-An (Andy) Chang</h2>
         <p className=" break-words text-2xl md:w-80">Web Developer ( Frontend / Backend )</p>
       </div>
     </div>

6db0ee7f8db503e22949fbad466b8fcf6f3c650b
Andy Chang <andy.chang@mycommunitydirectory.com.au>
2022-01-30 00:16:38 +1000

    bug fix project page

--- components/Projects.js
+++ components/Projects.js
@@ -27,13 +27,13 @@ function Projects() {
   return (
     <div className="py-10">
       <div className="px-4">
-        <h2 className="text-lg font-bold underline decoration-solid">
+        <h2 className="mt-80 md:mt-1 text-lg font-bold underline decoration-solid">
           Projects
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2">
           {projects.map((project, i) => (
             <div className="flex justify-center">
-            <div key={i} className="pt-10 ">
+            <div key={i} className="pt-10 cursor-pointer">
               <img className="m-10 h-40 w-80 rounded-lg sm:h-48 " src={project.image} />
               <div className="pt-5 font-bold flex justify-center">
                 <h3>{project.title}</h3>

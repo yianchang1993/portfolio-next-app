9db6e16d058199a6f206154b2561fc90898020ce
Andy Chang <andy.chang@mycommunitydirectory.com.au>
2022-01-30 13:03:21 +1000

    framer motion

--- components/Content.js
+++ components/Content.js
@@ -4,19 +4,24 @@ import AboutMe from '../components/AboutMe'
 import Work from '../components/Work'
 import Bio from '../components/Bio'
 import FindMe from '../components/FindMe'
+import { motion } from 'framer-motion'
 
 function Content() {
   return (
     <div className=" bg-backgoundColor ">
     {/* bg-red-50 */}
-      <div className="
-       mx-16 md:px-40 space-y-5 overflow-hidden">
+      <motion.div className="
+       mx-16 md:px-40 space-y-5 overflow-hidden"
+       initial={{opacity: 0,y:-40}}
+       animate={{opacity: 1,y:0}}
+       transition={{duration:2}}
+       >
         <Hero />
         <AboutMe />
         <Work/>
         <Bio/>
         <FindMe/>
-      </div>
+      </motion.div>
     </div>
   )
 }
--- components/Projects.js
+++ components/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import {projectsConsts} from '../constants/project'
-
+import { motion } from 'framer-motion'
 
 export const projects = [
   {
@@ -15,9 +15,10 @@ export const projects = [
   },
   {
     title: 'WebRTC App',
-    description: "Using React for single page front-end, socket.io for back-end to buidl React Video Chat Application using WebRTC.",
-      image: 'https://i.imgur.com/vvsAg99.png',
-      tags: ['React', 'WebRTC'],
+    description:
+      'Using React for single page front-end, socket.io for back-end to buidl React Video Chat Application using WebRTC.',
+    image: 'https://i.imgur.com/vvsAg99.png',
+    tags: ['React', 'WebRTC'],
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 2,
@@ -26,33 +27,37 @@ export const projects = [
 
 function Projects() {
   return (
-    <div className="py-10">
-    
+    <motion.div className="py-10"
+    initial={{opacity: 0,y:-40}}
+       animate={{opacity: 1,y:0}}
+       transition={{duration:2}}>
       <div className="px-4">
-        <h2 className="mt-80 md:mt-1 text-lg font-bold underline decoration-solid">
+        <h2 className="mt-80 text-lg font-bold underline decoration-solid md:mt-1">
           Projects
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2">
           {projects.map((project, i) => (
-            <div className="flex justify-center">
-            <div key={i} className="pt-10 cursor-pointer">
-              <img className="m-10 h-40 w-80 rounded-lg sm:h-48 " src={project.image} />
-              <div className="pt-5 font-bold flex justify-center">
-                <h3>{project.title}</h3>
+            <motion.div className="flex justify-center"
+            whileHover={{scale:1.1}}
+            >
+              <div key={i} className="cursor-pointer pt-10">
+                <img
+                  className="m-10 h-40 w-80 rounded-lg sm:h-48 "
+                  src={project.image}
+                />
+                <div className="flex justify-center pt-5 font-bold">
+                  <h3>{project.title}</h3>
+                </div>
+                <div className="ml-3 w-96">
+                  <p className="m-10 break-words">{project.description}</p>
+                </div>
               </div>
-              <div className="w-96 ml-3">
-                <p className="m-10 break-words">{project.description}</p>
-              </div>
-            </div>
-            </div>
-            
+            </motion.div>
           ))}
         </div>
       </div>
       <br />
-      
-    </div>
+    </motion.div>
   )
 }
-
 export default Projects

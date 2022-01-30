4446a1c6b082e85b39801b1b8057d90c5d8fe111
Andy Chang <andy.chang@mycommunitydirectory.com.au>
2022-01-30 14:14:03 +1000

    tech page

--- components/TechnologiesSection.js
+++ components/TechnologiesSection.js
@@ -1,7 +1,83 @@
-import React from 'react';
+import React from 'react'
+import { motion } from 'framer-motion'
 
 function TechnologiesSection() {
-  return <div>this is tech sec</div>;
+  return (
+    <div>
+      <div className="px-4 pt-48 md:px-20">
+        <h2 className="text-lg font-bold underline decoration-solid md:mt-1">
+          Tech Stack
+        </h2>
+        <div className="w-screen space-x-5 sm:flex">
+          <div className="py-10">
+            <ul className="grid w-80 grid-cols-2 sm:w-96 sm:grid-cols-3 ">
+              <motion.li whileHover={{ scale: 0.9 }}>
+                <div className="m-1 flex h-12 items-center justify-center rounded-lg bg-blue-200">
+                  HTML
+                </div>
+              </motion.li>
+              <motion.li whileHover={{ scale: 0.9 }}>
+                <div className="m-1 flex h-12 items-center justify-center rounded-lg bg-blue-200">
+                  JavaScript
+                </div>
+              </motion.li>
+              <motion.li whileHover={{ scale: 0.9 }}>
+                <div className="m-1 flex h-12 items-center justify-center rounded-lg bg-blue-200">
+                  CSS
+                </div>
+              </motion.li>
+              <motion.li whileHover={{ scale: 0.9 }}>
+                <div className="m-1 flex h-12 items-center justify-center rounded-lg bg-blue-200">
+                  TailwindCss
+                </div>
+              </motion.li>
+              <motion.li whileHover={{ scale: 0.9 }}>
+                <div className="m-1 flex h-12 items-center justify-center rounded-lg bg-blue-200">
+                  React
+                </div>
+              </motion.li>
+              <motion.li whileHover={{ scale: 0.9 }}>
+                <div className="m-1 flex h-12 items-center justify-center rounded-lg bg-blue-200">
+                  NEXT.js
+                </div>
+              </motion.li>
+              <motion.li whileHover={{ scale: 0.9 }}>
+                <div className="m-1 flex h-12 items-center justify-center rounded-lg bg-blue-200">
+                  Material UI
+                </div>
+              </motion.li>
+              <motion.li whileHover={{ scale: 0.9 }}>
+                <div className="m-1 flex h-12 items-center justify-center rounded-lg bg-blue-200">
+                  C#
+                </div>
+              </motion.li>
+              <motion.li whileHover={{ scale: 0.9 }}>
+                <div className="m-1 flex h-12 items-center justify-center rounded-lg bg-blue-200">
+                  ASP.NET
+                </div>
+              </motion.li>
+              <motion.li whileHover={{ scale: 0.9 }}>
+                <div className="m-1 flex h-12 items-center justify-center rounded-lg bg-blue-200">
+                  .Net Core
+                </div>
+              </motion.li>
+              <motion.li whileHover={{ scale: 0.9 }}>
+                <div className="m-1 flex h-12 items-center justify-center rounded-lg bg-blue-200">
+                  Microsoft Azure
+                </div>
+              </motion.li>
+            </ul>
+          </div>
+          <div className="sm:pl-48">
+            <img
+              className="h-80 w-80 md:w-96 md:h-96"
+              src="https://i.imgur.com/0PtuOzD.png"
+            ></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default TechnologiesSection;
+export default TechnologiesSection
--- components/TechnologyContent.js
+++ components/TechnologyContent.js
@@ -6,7 +6,6 @@ import { motion } from 'framer-motion'
 export default function TechnologyContent() {
   return (
     <div className=" bg-backgoundColor h-screen">
-    {/* bg-red-50 */}
       <motion.div className="
        mx-16 md:px-40 space-y-5 overflow-hidden"
        initial={{opacity: 0,y:-40}}
